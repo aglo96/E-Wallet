623cfd49b7d9ecd09bcf8583aee3cb5c647b5f9b
guocodes <awesomeag@hotmail.com>
2021-11-14 18:22:19 +0800

    initialize

---
+++ src/main/java/com/example/leon/ewallet/entity/Account.java
@@ -0,0 +1,65 @@
+package com.example.leon.ewallet.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="balance")
+    private int balance;
+
+
+    public Account() {
+    }
+
+    public Account(String name, String email, int balance) {
+        this.name = name;
+        this.email = email;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
